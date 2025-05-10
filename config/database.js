85bdfc66e3f1f8cb0b8a11bc16f1bc5a9f197460
unknown <seyiayobami14@gmail.com>
2025-05-10 21:14:05 +0100

    nothing of note

--- config/database.js
+++ config/database.js
@@ -3,11 +3,13 @@ import colors from 'colors'
 
 const connectDB = async () =>{
     try {
+        await console.log(process.env.PORT, process.env.MONGO_URI);
+        
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
         
     } catch (error) {
-        console.log(error);
+        console.log(error)
         process.exit(1)
     }
 }
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express, { urlencoded } from 'express'
-import router from './Routes/route.js'
+import router from './Routes/route.js';
 import { fileURLToPath } from 'url';
-import path from 'path'
-import cors from 'cors'
+import path from 'path';
+import cors from 'cors';
+// dotenv.config();
 import connectDB from './config/database.js';
 
+const PORT = process.env.PORT || 8082
 const app = express()
 app.use(cors())
-const PORT = process.env.PORT || 8082
 
 connectDB()
+console.log(process.env.MONGO_URI);
 
 //body parser
 app.use(express.json())
@@ -18,13 +20,10 @@ app.use(express.urlencoded({extended : false}))
 // routes 
 app.use('/api/jobs' , router)
 
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname, 'public')))
 
-
 app.listen(PORT, ()=>{
     console.log(`Gbera on port ${PORT}`);
-    
 })
